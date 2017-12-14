05257079f18604949b9dc5e419d45ec0f8bc0978
Filip Mularski <filipelblag@gmail.com>
2017-12-14 18:27:15 +0100

    Added MoviesController

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie { Name = "Alien"},
+                new Movie { Name = "Shrek"},
+                new Movie { Name = "The Last Jedi"},
+                new Movie { Name = "Pocahontas"},
+                new Movie { Name = "Interstellar"}
+            };
+
+            return View(movies[new Random().Next(0, 5)]);
+        }
+    }
+}
