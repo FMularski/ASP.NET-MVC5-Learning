a15b0428f17984029082d6a7869fcfaf4e46cbc7
Filip Mularski <filipelblag@gmail.com>
2017-12-19 21:26:22 +0100

    A lot of stuff. Finished on ViewModels

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,29 +4,78 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Index()
+        //public ActionResult Random()
+        //{
+        //    List<Movie> movies = new List<Movie>
+        //    {
+        //        new Movie { Name = "Alien"},
+        //        new Movie { Name = "Shrek"},
+        //        new Movie { Name = "The Last Jedi"},
+        //        new Movie { Name = "Pocahontas"},
+        //        new Movie { Name = "Interstellar"}
+        //    };
+
+        //    return View(movies[new Random().Next(0, 5)]);
+        //}
+
+        public ActionResult Edit(int id)
         {
-            return View();
+            return Content("id = " + id);
         }
 
-        public ActionResult Random()
+        public ActionResult Foo(int arg)
         {
-            List<Movie> movies = new List<Movie>
+            if (arg % 2 == 0)
+                return RedirectToAction("Edit", "Movies", new { id = arg });
+            else
+                return Redirect("http://google.com");
+        }
+
+        public ActionResult Index( int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue) pageIndex = 1;
+            if (string.IsNullOrEmpty(sortBy)) sortBy = "Name";
+
+            return Content(string.Format("pageIndex = {0}, sortBy = {1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate( int? year, int? month)
+        {
+            if (!year.HasValue) year = 2000;
+            if (!month.HasValue) month = 1;
+
+            return Content(string.Format("Date: {0}/{1}", month, year));
+        }
+
+        public ViewResult Random()
+        {
+            var customers = new List<Customer>
             {
-                new Movie { Name = "Alien"},
-                new Movie { Name = "Shrek"},
-                new Movie { Name = "The Last Jedi"},
-                new Movie { Name = "Pocahontas"},
-                new Movie { Name = "Interstellar"}
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"},
+                new Customer { Name = "Customer 3"}
             };
 
-            return View(movies[new Random().Next(0, 5)]);
+            var movie = new Movie
+            {
+                Name = "The Last Jedi"
+            };
+
+            var VM = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(VM);
         }
+
     }
 }
