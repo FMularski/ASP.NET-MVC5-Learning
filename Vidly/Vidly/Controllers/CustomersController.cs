cdaddebb4875d1caa195d00a46df428cde04a92b
Filip Mularski <filipelblag@gmail.com>
2017-12-21 21:03:25 +0100

    ApplicationDbContext added.

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -9,25 +9,34 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        List<Customer> customers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith"},
-                new Customer { Id = 2, Name = "Mary Williams"}
-            };
+        private ApplicationDbContext _Context;
+
+        public CustomersController()
+        {
+            _Context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _Context.Dispose();
+        }
 
         public ViewResult Index()
         {
+            var customers = _Context.Customers.ToList();
+
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = customers.Find(c => c.Id == id);
+            var customer = _Context.Customers.SingleOrDefault(c => c.Id == id);
 
-            if (customer != null)
-                return View(customer);
-            else
+            if (customer == null)
                 return HttpNotFound();
+
+            return View(customer);
         }
+
     }
 }
