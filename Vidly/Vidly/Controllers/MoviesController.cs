a5bc4329700ed8aaf782064259d621bd9e611bd1
Filip Mularski <filipelblag@gmail.com>
2017-12-21 17:14:12 +0100

    First migrations

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -10,71 +10,68 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        //public ActionResult Random()
+        //public ActionResult Edit(int id)
         //{
-        //    List<Movie> movies = new List<Movie>
-        //    {
-        //        new Movie { Name = "Alien"},
-        //        new Movie { Name = "Shrek"},
-        //        new Movie { Name = "The Last Jedi"},
-        //        new Movie { Name = "Pocahontas"},
-        //        new Movie { Name = "Interstellar"}
-        //    };
-
-        //    return View(movies[new Random().Next(0, 5)]);
+        //    return Content("id = " + id);
         //}
 
-        public ActionResult Edit(int id)
+        //public ActionResult Foo(int arg)
+        //{
+        //    if (arg % 2 == 0)
+        //        return RedirectToAction("Edit", "Movies", new { id = arg });
+        //    else
+        //        return Redirect("http://google.com");
+        //}
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue) pageIndex = 1;
+        //    if (string.IsNullOrEmpty(sortBy)) sortBy = "Name";
+
+        //    return Content(string.Format("pageIndex = {0}, sortBy = {1}", pageIndex, sortBy));
+        //}
+
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        //public ActionResult ByReleaseDate( int? year, int? month)
+        //{
+        //    if (!year.HasValue) year = 2000;
+        //    if (!month.HasValue) month = 1;
+
+        //    return Content(string.Format("Date: {0}/{1}", month, year));
+        //}
+
+        //public ViewResult Random()
+        //{
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name = "Customer 1"},
+        //        new Customer { Name = "Customer 2"},
+        //        new Customer { Name = "Customer 3"}
+        //    };
+
+        //    var movie = new Movie
+        //    {
+        //        Name = "The Last Jedi"
+        //    };
+
+        //    var VM = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    return View(VM);
+        //}
+
+        List<Movie> Movies = new List<Movie>
         {
-            return Content("id = " + id);
-        }
+            new Movie { Id = 1, Name = "The Force Awekening" },
+            new Movie { Id = 2, Name = "The Last Jedi"}
+        };
 
-        public ActionResult Foo(int arg)
+        public ViewResult Index()
         {
-            if (arg % 2 == 0)
-                return RedirectToAction("Edit", "Movies", new { id = arg });
-            else
-                return Redirect("http://google.com");
-        }
-
-        public ActionResult Index( int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue) pageIndex = 1;
-            if (string.IsNullOrEmpty(sortBy)) sortBy = "Name";
-
-            return Content(string.Format("pageIndex = {0}, sortBy = {1}", pageIndex, sortBy));
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate( int? year, int? month)
-        {
-            if (!year.HasValue) year = 2000;
-            if (!month.HasValue) month = 1;
-
-            return Content(string.Format("Date: {0}/{1}", month, year));
-        }
-
-        public ViewResult Random()
-        {
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1"},
-                new Customer { Name = "Customer 2"},
-                new Customer { Name = "Customer 3"}
-            };
-
-            var movie = new Movie
-            {
-                Name = "The Last Jedi"
-            };
-
-            var VM = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(VM);
+            return View(Movies);
         }
 
     }
